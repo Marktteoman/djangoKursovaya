f12c54864274d10f4e54d9d22bbf404a4a577466
Mark <toksunteo@mail.ru>
2024-06-19 20:36:31 +0300

    картиночки

--- project/CS50Shop/CS50Shop/settings.py
+++ project/CS50Shop/CS50Shop/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 # LOGIN_REDIRECT_URL = reverse_lazy('ShopDegenerative:profile')
--- project/CS50Shop/CS50Shop/urls.py
+++ project/CS50Shop/CS50Shop/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from django.conf.urls.static import static
+from django.conf import settings
 
 from CS50Shop import settings
 from django.urls import path, include
@@ -10,6 +11,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ShopDegenerative.urls', namespace="ShopDegenerative")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
